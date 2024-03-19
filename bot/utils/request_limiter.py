7f4f51db1c4317681e1d916e9c1a9b0a7829f311
Zhansen <supersegizbay@gmail.com>
2024-03-19 12:27:24 +0600

    fix: set expiry on requests

--- bot/utils/request_limiter.py
+++ bot/utils/request_limiter.py
@@ -13,13 +13,12 @@ def rate_limited(max_requests: int = 10):
             user_id: int = message.from_user.id
 
             request_count = await redis_client.incr(user_id)
+            await redis_client.expire(user_id, 3600)
 
             if request_count > max_requests:
                 username = message.from_user.username
                 logger.warning(f'User {username} exceeded limit')
                 await message.reply(LEXICON_RU['limit_exceeded'])
-
-                await redis_client.expire(user_id, 3600)
                 return
 
             return await func(message, *args, **kwargs)
